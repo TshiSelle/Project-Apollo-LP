d95a4c474d89772b2fcb3f0c78950228dcafbeff
OmarHoussami <omar.houssami20@gmail.com>
2022-04-08 15:56:09 +0300

    edits

--- apollo/src/components/Header/MyHeader.jsx
+++ apollo/src/components/Header/MyHeader.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import './header.css';
-import HeaderImg from '../../assets/headerimg.jpg';
 
 const MyHeader = () => {
   return (
     <section id='header'>
-      <div className='container__header'>
-        <div className='title'>
-          <h1>Welcome to Project Apollo</h1>
-          <hr className='breakLine' />
-        </div>
-        <div className='header__background'></div>
-        <div className='header__definition'>
-          <p>
-            Start Bootstrap can help you build better websites using the
-            Bootstrap framework! Just download a theme and start customizing, no
-            strings attached!
-          </p>
-          <a href='#about'>Find Out More</a>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-12 '>Column</div>
+          <div className='col-lg-12  '>Column</div>
+          <div className='col-lg-12  '>Column</div>
         </div>
       </div>
     </section>
--- apollo/src/index.js
+++ apollo/src/index.js
@@ -4,4 +4,5 @@ import App from './App'
 import React from 'react';
 
 
+
 ReactDOM.render(<App />, document.querySelector("#root"))
